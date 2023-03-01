9416f6b58286d8677e5d77de3135e584cd71710f
andrei1234D <balasandrei200@yahoo.com>
2023-03-01 19:55:45 +0200

    search (...)

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,7 +1,33 @@
 import '../style/Search.css';
-
+import { Button } from '@mui/material';
+import TextField from '@mui/material/TextField';
 function Search() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="centerSearch">
+        <TextField
+          id="filled-error-helper-text"
+          label="Search field"
+          variant="filled"
+          color="secondary"
+          size="small"
+        />
+      </div>
+      <div className="centerSearch">
+        <Button
+          variant="contained"
+          type="submit"
+          sx={{
+            width: '100px',
+            backgroundColor: '#2FA4FF',
+            fontSize: 'small',
+          }}
+        >
+          Search
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default Search;
